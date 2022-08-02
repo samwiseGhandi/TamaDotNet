d1233d2d7900976963f34d86439081708fe862c0
Sheikthecatman <fedronee@gmail.com>
2022-08-02 18:23:49 +0200

    Rewrite boiler-plate ApplicationDbContext

--- TamaDotNet/Server/Data/ApplicationDbContext.cs
+++ TamaDotNet/Server/Data/ApplicationDbContext.cs
@@ -1,14 +1,12 @@
-﻿using Duende.IdentityServer.EntityFramework.Options;
-using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using TamaDotNet.Server.Models;
+﻿
 
 namespace TamaDotNet.Server.Data {
-    public class ApplicationDbContext:ApiAuthorizationDbContext<ApplicationUser> {
+    public class ApplicationDbContext:IdentityDbContext<ApplicationUser> {
         public ApplicationDbContext(
-            DbContextOptions options,
-            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions) {
+            DbContextOptions options) : base(options) {
         }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
     }
 }
