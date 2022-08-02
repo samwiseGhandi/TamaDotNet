a4edc9bb24ac08bf67df73b45e52ed02515d8253
Sheikthecatman <fedronee@gmail.com>
2022-08-02 18:23:04 +0200

    add globalusings to server & client

---
+++ TamaDotNet/Client/globalusings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.AspNetCore.Components.Authorization;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using TamaDotNet.Client;
+global using Blazored.LocalStorage;
--- TamaDotNet/Server/GlobalUsings.cs
+++ TamaDotNet/Server/GlobalUsings.cs
@@ -1,1 +1,17 @@
-﻿global using TamaDotNet.Shared.Data;
+﻿global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.IdentityModel.Tokens;
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using System.Text;
+
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore;
+
+global using TamaDotNet.Server.Models;
+global using TamaDotNet.Server.Data;
+global using TamaDotNet.Shared.Data;
